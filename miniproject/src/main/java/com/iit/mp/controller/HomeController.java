517e9b50b998427888cb4d8bbe7c90058546b5fd
admin <dbsaud3112@naver.com>
2023-03-10 11:26:14 +0900

    프로젝트 생성

---
+++ miniproject/src/main/java/com/iit/mp/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.iit.mp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class HomeController {
+	
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		return "home";
+	}
+	
+}
---
+++ miniproject/src/main/java/com/iit/mp/dto/MemberDto.java
@@ -0,0 +1,14 @@
+package com.iit.mp.dto;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
+public class MemberDto {
+	private String mbrId, mbrPw, mbrNm, mbrMblTelno, mbrHomeAddr, mbrHomeDaddr;
+	private Date mbrBrdt, mbrRegYmd;
+}
